b05a6fb3821f23ce52d5e05d711ae338cca5813d
Alexandros Andriopoulos <alex.andriopoulos@live.com>
2025-04-27 16:28:25 +0300

    simplify with sqlalchemy - TODO: log events and errors

--- cli.py
+++ cli.py
@@ -1,13 +1,10 @@
-import psycopg2
 from logging_setup import logging_setup, logging
 from argparse import ArgumentParser
-from urllib.parse import urlparse
 import pandas as pd
+from sqlalchemy import create_engine, text
 
 logger = logging.getLogger(__name__)
 logging_setup(logger)
-# logger.info("hi")
-# logger.debug("not hi")
 
 # set up argument parsing
 parser = ArgumentParser()
@@ -19,57 +16,10 @@ args = parser.parse_args()
 
 
 def pg_connect_n_load(uri):
-    # if db_name := get_db_name(uri):
-    #     logger.info(f"Connecting to database {db_name}")
-    # else:
-    #     logger.info(f'Invalid URI')
-    # Sample URI (you can replace this with your actual connection string)
-    # uri = "postgresql://postgres:Password21!!!@localhost:5432/pagila"
-
-    try:
-        # Parse the URI
-        parsed_uri = urlparse(uri)
-        # Extract the components
-        username = parsed_uri.username
-        password = parsed_uri.password
-        host = parsed_uri.hostname
-        port = parsed_uri.port
-        database = parsed_uri.path[1:]  # Remove leading slash
-        logger.info(f'Connecting to {database}')
-    except Exception as e:
-        logger.error("Error: " + str(e))
-
-    # Connect to PostgreSQL
-    try:
-        connection = psycopg2.connect(
-            dbname=database,
-            user=username,
-            password=password,
-            host=host,
-            port=port,
-        )
-        logger.info("Connection established!")
-
-        # Example query
-        # cursor = connection.cursor()
-        # cursor.execute("SELECT version();")
-        # cursor.execute(
-        #     """
-        #     Select *
-        #     from payment
-
-        #     """
-        # )
-        # result = cursor.fetchmany(10)
-        # logger.info('Query results:')
-        # for line in result:
-        #     print(line)
-        # logger.info('End of results')
-        logger.info(f'Disconnecting from {database}')
-        cursor.close()
-        connection.close()
-    except Exception as e:
-        logger.error(f"Error: {e}")
+    engine = create_engine(uri)
+    with engine.connect() as conn, conn.begin():
+        df = pd.read_sql_table("payment", conn)
+    return df
 
 
 def csv_load(csv_file):
@@ -81,9 +31,6 @@ def csv_load(csv_file):
         logger.error(e)
 
 
-# def get_db_name(uri):
-#     return re.search(r'(?<=/)\w+$', uri).group()
-
 if args.db_connection and args.path:
     df = pg_connect_n_load(args.path)
 elif args.csv_path and args.path:
@@ -91,3 +38,5 @@ elif args.csv_path and args.path:
 else:
     logger.warning("Invalid parameters, loading default dataset")
     df = csv_load("data/KaggleV2-May-2016.csv")
+
+print(df.head())
